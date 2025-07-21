eecfde6406a0366066bfb1f04fcc7cf954545ab3
viodid <david.yunta.aller@gmail.com>
2025-07-21 14:42:30 +0200

    read EEPROMT byte helper

--- sketch_jun20a/sketch_jun20a.ino
+++ sketch_jun20a/sketch_jun20a.ino
@@ -7,30 +7,37 @@
 #include <stdio.h>
 
 void setAddress(int add, bool outEnable) {
-  pinMode(SHIFT_DATA, OUTPUT);
-  pinMode(SHIFT_CLK, OUTPUT);
-  pinMode(SHIFT_LATCH, OUTPUT);
-
   shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, outEnable ? (add >> 8 | 1 << 8) : (add >> 8));
   shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, add);
+
   digitalWrite(SHIFT_LATCH, LOW);
   digitalWrite(SHIFT_LATCH, HIGH);
   digitalWrite(SHIFT_LATCH, LOW);
 }
 
-byte readEEPROM(int add) {
-  setAddress(1024, true);
+/*
+ * Read a byte from the EEPROM at the specified address.
+ */
+byte readEEPROM(int address) {
+  for (int pin = EEPROM_D0; pin <= EEPROM_D7; pin++) {
+    pinMode(pin, INPUT);
+  }
+  setAddress(address, true);
+
   byte data = 0;
-  for (int i = EEPROM_D7; i >= EEPROM_D0; i--) {
-      data = (data << 1) + digitalRead(i);
+  for (int pin = EEPROM_D7; pin >= EEPROM_D0; pin -= 1) {
+    data = (data << 1) + digitalRead(pin);
   }
   return data;
 }
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(SHIFT_DATA, OUTPUT);
+  pinMode(SHIFT_CLK, OUTPUT);
+  pinMode(SHIFT_LATCH, OUTPUT);
   Serial.begin(57600);
-  Serial.println(readEEPROM(1));
+  Serial.println(readEEPROM(3));
 }
 
 void loop() {
