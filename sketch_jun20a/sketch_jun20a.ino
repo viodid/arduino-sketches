883ee41bd57acf045b47c2feafc0cfe1c71ad5b2
viodid <david.yunta.aller@gmail.com>
2025-07-23 14:59:20 +0200

    print EEPROM first 255 addr

--- sketch_jun20a/sketch_jun20a.ino
+++ sketch_jun20a/sketch_jun20a.ino
@@ -36,8 +36,20 @@ void setup() {
   pinMode(SHIFT_DATA, OUTPUT);
   pinMode(SHIFT_CLK, OUTPUT);
   pinMode(SHIFT_LATCH, OUTPUT);
+
   Serial.begin(57600);
-  Serial.println(readEEPROM(3));
+
+  for (int base = 0; base < 255; base+=16) {
+    byte data[16];  
+    for (int offset = 0; offset < 16; offset++) {
+      data[offset] = readEEPROM(offset + base);
+    }
+    char buf[80];
+    sprintf(buf, "%03x: %02x %02x %02x %02x %02x %02x %02x %02x\t%02x %02x %02x %02x %02x %02x %02x %02x",
+        base, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9],
+        data[10], data[11], data[12], data[13], data[14], data[15]);
+    Serial.println(buf);
+  }
 }
 
 void loop() {
